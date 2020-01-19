cb79406155e3ea6162ea14e35450741478c94b36
QusaiHroub <qusaihroub.r@gmail.com>
2020-01-19 10:54:04 +0200

    update

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -12,25 +12,31 @@ export class MenuComponent implements OnInit {
     cityName: string;
     cities: any = <any>[];
     citiesObj: any = <any>{};
+    pos: any = <any>{};
 
     @Input() display: string = "none";
     @Output() locChanged = new EventEmitter<string>();
 
     constructor(private weatherService: WeatherService, private el: ElementRef) {
-      this.searchCities();
+        this.searchCities();
     }
 
     ngOnInit() {
     }
 
     searchCities() {
-      this.weatherService.findN('31','35')
-        .subscribe(data => {
-          this.citiesObj = data;
-          this.cities = this.citiesObj.list;
-        }, err => {
-            alert('Failed to get weather.' );
-        }, () => {})
+      navigator.geolocation.getCurrentPosition(pos => {
+          this.pos.lat = pos.coords.latitude.toString();
+          this.pos.lng = pos.coords.longitude.toString();
+          this.weatherService.findN(this.pos.lat, this.pos.lng)
+            .subscribe(data => {
+              this.citiesObj = data;
+              this.cities = this.citiesObj.list;
+            }, err => {
+                alert('Failed to get weather.' );
+            }, () => {})
+      });
+
     }
 
     onLocChanged(newLoc: string) {
